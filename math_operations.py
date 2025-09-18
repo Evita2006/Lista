b41d50be04f74da43eb0e1e4b207dfeed7e6f292
Evita2006 <evamarti@cabanell.es>
2025-09-18 19:32:02 +0200

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,27 @@
+def modulo(a, b):
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    division = a // b
+    mult = division * b
+    subs = a - mult
+    print("The remainder is: ", subs)
+    return subs
+
+def potencia(x, y):
+    resultado = 1
+    for _ in range(y):
+        resultado *= x
+    return resultado
+
+def raiz_cuadrada_entera(x):
+    if x < 0:
+        raise ValueError('No existe raíz cuadrada entera para este valor')
+
+    if x == 0 or x == 1:
+        return x
+
+    i = 1
+    while i * i <= x:
+        i += 1
+
+    return i - 1
